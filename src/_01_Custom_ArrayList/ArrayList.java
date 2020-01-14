9054667190d6b426f4c9e3a8ef7eb54220d252c4
League Student <league@iMac-14.attlocal.net>
2020-01-13 18:29:56 -0800

    started module 1

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -3,17 +3,19 @@ package _01_Custom_ArrayList;
 @SuppressWarnings("unchecked")
 
 public class ArrayList <T>{
-	
+	T[] cool;
 	public ArrayList() {
+		cool = (T[]) new Object [0];
+
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
 		
-		return null;
+		return cool [loc];
 	}
 	
 	public void add(T val) {
-		
+		T[] cool1 = (T[]) new Object [cool.length + 1];
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
@@ -25,11 +27,17 @@ public class ArrayList <T>{
 	}
 	
 	public void remove(int loc) throws IndexOutOfBoundsException {
-		
+		T[] cool2 = (T[]) new Object [cool.length - 1];
 	}
 	
 	public boolean contains(T val) {
 		
 		return false;
+	}
+
+	public int size() {
+		// TODO Auto-generated method stub
+	
+		return 0;
 	}
 }
